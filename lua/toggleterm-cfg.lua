ab6f64c5b773221504993743bed324046e5c72dd
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-12-13 18:29:36 +0100

    +ToggleTerm working and set

--- lua/toggleterm-cfg.lua
+++ lua/toggleterm-cfg.lua
@@ -9,9 +9,29 @@ local toggleterm_status_ok, toggleterm = pcall(require, "toggleterm")
 if not toggleterm_status_ok then
 	return
 end
-
 vim.o.hidden = true
-toggleterm.setup()
+
+-- Default ToggleTerm terminal
+toggleterm.setup{
+	direction = 'float',
+	float_opts = {
+		border = 'single'
+	}
+}
+vim.keymap.set({'n', 'i'}, '<Leader>t', '<ESC>:ToggleTerm<CR>')
+vim.keymap.set({'n', 'i'}, '<Leader>T', '<ESC>:ToggleTerm direction=horizontal<CR>')
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n><C-w><C-w>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 
 ------
 -- Toggleterm-manager configuration
@@ -21,4 +41,3 @@ if not manager_status_ok then
 	return
 end
 toggleterm_manager.setup()
-print "toggle manager"
